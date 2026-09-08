0c6e607e4a7a40019e73685f38ac72b6f3af2785
dmca-jaipur <131617180+dmca-jaipur@users.noreply.github.com>
2026-09-08 15:47:45 +0530

    Update route.ts

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { crypto } from 'crypto';
+import crypto from 'crypto';
 
 export async function POST(req: Request) {
   const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
